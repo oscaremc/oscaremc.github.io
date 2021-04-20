1726542a8d336422e710edbb489ba6b9991a771e
OscarMontalvo <oscaremontalvoc12@hotmail.com>
2021-04-20 03:28:09 -0500

    se arreglan archivos

--- reto1-DashboardEmpresarial/dashboards/apps.py
+++
@@ -1,9 +0,0 @@
-"""Posts application module"""
-
-from django.apps import AppConfig
-
-
-class DashboardsConfig(AppConfig):
-    """Application settings"""
-    name = 'dashboards'
-    verbose_name ='Posts'
